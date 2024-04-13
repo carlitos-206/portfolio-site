e9ce523a27a9726453ffb11838b84cf698aa4482
carlitos-206 <carloscaceres041@gmail,com>
2024-04-13 03:11:12 -0700

    mobile nav added

--- src/global/components/nav-bar/index.jsx
+++ src/global/components/nav-bar/index.jsx
@@ -1,29 +1,42 @@
 import React from "react";
-import "./index.css"
-const NavBar = () => {
-    let windowWidth = window.innerWidth;
-    if(windowWidth > 768){
-    return (
+import { useLocation } from "react-router-dom";
+import { BsPersonVcardFill } from "react-icons/bs";
+import { GoProjectRoadmap } from "react-icons/go";
+import { ImProfile } from "react-icons/im";
+import { MdWork } from "react-icons/md";
+import { MdOutlineConnectWithoutContact } from "react-icons/md";
+import { MdHome } from "react-icons/md";
 
+import "./index.css";
+
+const NavBar = () => {
+    const location = useLocation();
+    const windowWidth = window.innerWidth;
+
+    const renderNavItem = (path, IconComponent, label) => {
+        // Determines if the icon should render based on the current path
+        if (location.pathname !== path) {
+            return (
+                <h1 className="nav-item" onClick={() => window.location.href = path}>
+                    {windowWidth > 768 ? label : <IconComponent />}
+                </h1>
+            );
+        }
+        return null;  // Don't render the icon for the current path
+    };
+
+    return (
         <div className="navbar-container">
-        <h1 className="nav-item" onClick={(e)=>{window.location.href = '/about' }} >About</h1>
-        <h1 className="nav-item" onClick={(e)=>{window.location.href = '/projects'}}>Projects</h1>
-        <h1 className="nav-item">Resume</h1>
-        <h1 className="nav-item">Work</h1>
-        <h1 className="nav-item">Contact</h1>
+            {renderNavItem('/about', BsPersonVcardFill, 'About')}
+            {renderNavItem('/projects', GoProjectRoadmap, 'Projects')}
+            {renderNavItem('/resume', ImProfile, 'Resume')}
+            {location.pathname !== '/' && <h1 className="nav-item" onClick={() => window.location.href = '/'}>
+                {windowWidth > 768 ? 'Home' : <MdHome />}
+            </h1>}
+            {renderNavItem('/work', MdWork, 'Work')}
+            {renderNavItem('/contact', MdOutlineConnectWithoutContact, 'Contact')}
         </div>
     );
-    }else{
-        return (
-            <div className="navbar-container">
-            <h1 className="nav-item" onClick={(e)=>{window.location.href = '/about' }} >About</h1>
-            <h1 className="nav-item">Projects</h1>
-            <h1 className="nav-item">Resume</h1>
-            <h1 className="nav-item">Work</h1>
-            <h1 className="nav-item">Contact</h1>
-            </div>
-        );
-    }
 }
 
 export default NavBar;
